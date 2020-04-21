040e5bdd2de87f3089e9abadc1522e3c016e19cc
tjddus <tjddus424@naver.com>
2020-04-21 16:58:11 +0900

    tjddus: create admin user, add comments

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+admin.site.register(Post) # 관리자 페이지에서 'Post' 모델 확인
+
--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,26 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+
+# 장고 ORM , 쿼리셋(QuerySets)
+'''
+    from blog.models import Post
+    from django.contrib.auth.models import User
+    me = User.objects.get(username = 'tjddus')
+
+    Post.objects.create(author=me, title='title 01', text='text 01')
+    Post.objects.create(author=me, title='title 02', text='text 02')
+    Post.objects.all()
+    Post.objects.filter(author=me)
+    Post.objects.filter(title__contains='title')
+    Post.objects.filter(published_date__lte=timezone.now())
+    # __lte : 같거나 보다 작다
+    # __gte : 같거나 보다 크다
+    post = Post.objects.get(title='title 01')
+    post.publish()
+    Post.objects.order_by('created_date')
+    Post.objects.order_by('-created_date')
+
+
+
+'''
