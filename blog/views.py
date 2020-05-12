019aae9a08df4e3005052059859b7aa2c269ca64
tjddus <tjddus424@naver.com>
2020-05-13 03:03:42 +0900

    Design CSS

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(req):
-    return render(req, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(req, 'blog/post_list.html', {'posts': posts})
+    # req에 의해서 html이 전달되어졌다는 것
