390ab586e2856e4db77aa25de92f8c0265afd58a
tjddus <tjddus424@naver.com>
2020-05-27 20:03:55 +0900

    tjddus :add post_detail url, html

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -7,3 +7,8 @@ def post_list(req):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(req, 'blog/post_list.html', {'posts': posts})
     # req에 의해서 html이 전달되어졌다는 것
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
